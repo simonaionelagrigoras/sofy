ec4952b3e0bd1bb8aabf73f026f21f0a67059952
Grigoras Marian <grigorasmariang@gmail.com>
2019-03-23 04:07:13 +0200

    validare cod pe user guide si login

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>Sofy</title>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="static/css/main.css" />
     <script src="static/js/main.js"></script>
     <!-- Used for responsive design in order to control dimensions and scaling -->
@@ -20,21 +20,21 @@
                 <h3>Create an account:</h3>
                 <form action="/action_page.php" method="post">
                     <div class="fieldset required">
-                        <label for="email">Name</label>
-                        <input type="text" class="form-control" placeholder="Email address" name="email">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" class="form-control" placeholder="Email address" name="name">
                     </div>
                     <div class="fieldset required">
                         <label for="email">Email address</label>
-                        <input type="text" class="form-control" placeholder="Email address" name="email">
+                        <input type="text" id="email" class="form-control" placeholder="Email address" name="email">
                     </div>
                     <div class="fieldset required">
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" placeholder="Password" name="password"
+                        <input type="password" id="password" class="form-control" placeholder="Password" name="password"
                                minlength="8" required>
                     </div>
                     <div class="fieldset required">
                         <label for="confirm-password">Confirm password</label>
-                        <input type="password" class="form-control" placeholder="Password" name="confirm-password"
+                        <input type="password" id="confirm-password" class="form-control" placeholder="Password" name="confirm-password"
                                minlength="8" required>
                     </div>
                     <input type="submit" class="btn btn-md" value="Sign In">
@@ -43,10 +43,10 @@
             <div class="col-sm-6">
                 <h3>Login with existing account:</h3>
 				<form action="/action_page.php" method="post">
-					<label for="email">Email address</label>
-					<input type="text" class="form-control" placeholder="Email address" name="email">
-					<label for="password">Password</label>
-					<input type="password" class="form-control" placeholder="Password" name="password"
+					<label for="email_login">Email address</label>
+					<input type="text" id="email_login" class="form-control" placeholder="Email address" name="email">
+					<label for="password_login">Password</label>
+					<input type="password" id="password_login" class="form-control" placeholder="Password" name="password"
 						   minlength="8" required>
 					<input type="submit" class="btn btn-md" value="Sign Up">
 				</form>
--- static/js/main.js
+++ static/js/main.js
@@ -64,4 +64,8 @@ $('document').ready(function(){
             }
 		})
 	})
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 });
