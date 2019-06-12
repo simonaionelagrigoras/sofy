b8c9db68c81abddcba0a1544ccfbf2a3e46218c5
grig0 <simona.ionela.grigoras@gmail.com>
2019-06-13 00:08:51 +0300

    static pages improvements

--- Views/Help/index.php
+++ Views/Help/index.php
@@ -6,8 +6,12 @@
  * Time: 22:36
  */
 ?>
+<img width="300" style="float:left" src="/static/media/images/help.jpg">
+<div>
+    <p>In order to create repositories in our platform, you need to create an account</p>
+    <h2>How to create an account?</h2>
 
-<h1>How to make an account</h1>
+    <p>To create an account click on this <a href="/user/login/">link</a> and fill the registration form</p>
+    <p>After creating an account, for more details you can check our <a href="/guide/index">user guide</a></p>
+</div>
 
-
-<p>To make an account click on this <a href="http://sofy.local/user/login/">link</a></p>
--- Views/Homepage/index.php
+++ Views/Homepage/index.php
@@ -1,1 +1,13 @@
-<h1>Homepage content</h1>
+<div class="home">
+    <h1>Wellcome to Sofy</h1>
+    <div class="left">
+        <img src="/static/media/images/homepage-img.png">
+    </div>
+    <div class="right">
+        <h2>The best of Sofy</h2>
+        <p>As a repository administrator you can easily manage your repository through the web user interface</p>
+        <p>Organize your content in a structure of collections and by tags management.</p>
+        <p>Search in your content by name or by tags</p>
+        <p>Try it Now. Sign in <a href="/user/login">here</a>!</p>
+    </div>
+</div>
